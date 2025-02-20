0f5ce172e982764783afc6c53c998dc96103a49b
Christian Babin <cssbabin@gmail.com>
2025-02-19 23:16:08 -0500

    Moved old code to this repo

---
+++ lib/dart_gov_fbi.dart
@@ -0,0 +1,13 @@
+// @author Christian Babin
+// @version 0.1.2
+// https://github.com/babincc/dart_gov_fbi/blob/master/lib/dart_gov_fbi.dart
+
+export 'package:dart_gov_fbi/domain/models/fbi_image.dart';
+export 'package:dart_gov_fbi/features/art_crime/domain/models/art_crime.dart';
+export 'package:dart_gov_fbi/features/art_crime/domain/models/art_crime_result_set.dart';
+export 'package:dart_gov_fbi/features/art_crime/domain/repos/art_crime_repo.dart';
+export 'package:dart_gov_fbi/features/wanted_person/domain/models/wanted_person.dart';
+export 'package:dart_gov_fbi/features/wanted_person/domain/models/wanted_person_coordinates.dart';
+export 'package:dart_gov_fbi/features/wanted_person/domain/models/wanted_person_file.dart';
+export 'package:dart_gov_fbi/features/wanted_person/domain/models/wanted_person_result_set.dart';
+export 'package:dart_gov_fbi/features/wanted_person/domain/repos/wanted_person_repo.dart';
